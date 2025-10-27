7011244942a8c89162de2ae8a1b9a6c101e81427
João Oliveira <86302934+JoaoOliveiraaa@users.noreply.github.com>
2025-10-27 14:47:26 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -29,10 +29,10 @@ export async function middleware(request: NextRequest) {
 
   if (isProtectedRoute) {
     const {
-      data: { session },
-    } = await supabase.auth.getSession()
+      data: { user },
+    } = await supabase.auth.getUser()
 
-    if (!session?.user) {
+    if (!user) {
       const url = request.nextUrl.clone()
       url.pathname = "/auth/login"
       url.searchParams.set("redirect", request.nextUrl.pathname)
